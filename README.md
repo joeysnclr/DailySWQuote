bf4bd17b5371ab4cbe3fe4281873a3eb8b01c611
Joseph Sinclair <joey.sinclair02@gmail.com>
2020-05-24 04:26:47 +0000

    added functinoality

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DailySWQuote
+
+Python application that send me a text message with a random Star Wars quote using the Twilio API.
--- sendQuote.py
+++ sendQuote.py
@@ -1,2 +1,17 @@
-import twilio, os
+from twilio.rest import Client
+import os, requests
 
+account_sid = os.environ['TWILIO_SID']
+auth_token = os.environ['TWILIO_TOKEN']
+client = Client(account_sid, auth_token)
+
+r = requests.get("http://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote")
+quote = r.json()['starWarsQuote']
+
+
+
+message = client.messages.create(
+         body=quote,
+         from_=os.environ['TWILIO_PHONENUM'],
+         to=os.environ['MY_PHONENUM']
+     )
